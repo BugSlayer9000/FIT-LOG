705c1494da436e7c4ec7d0f18dca95b2ad47162f
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-02-19 13:20:47 +0000

    Fix path for static files in production and correct development environment typo in package.json

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/api/exercises", router);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get(/.*/,(_,res) => {
     res.sendFile(path.resolve(__dirname,"frontend/dist/index.html"))
