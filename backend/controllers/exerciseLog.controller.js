c23ddba0d1c9818f86959e8cb06692e225af2d7a
samod <83113845+BugSlayer9000@users.noreply.github.com>
2026-04-18 20:45:34 +0100

    Refactor exercise log routes: streamline route handlers by utilizing controller functions for improved readability and maintainability

---
+++ backend/controllers/exerciseLog.controller.js
@@ -0,0 +1,86 @@
+import ExerciseLog from "../models/exerciseLog.model.js";
+import mongoose from "mongoose";
+
+export const getAllLogs = async (req, res) => {
+  try {
+    const logs = await ExerciseLog.find().sort({ date: -1 });
+    res.status(200).json({ success: true, data: logs });
+  } catch (error) {
+    console.log("Error in getAllLogs", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const getLogsForOneExercise = async (req, res) => {
+  const { exerciseId } = req.params;
+  try {
+    const logs = await ExerciseLog.find({ exercise: exerciseId });
+    if (!logs) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Logs are not found" });
+    }
+    res.status(200).json({ success: true, data: logs });
+  } catch (error) {
+    console.log("Error in get log", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const createALog = async (req, res) => {
+  const log = req.body;
+
+  // add user validation here
+  if (!log.exercise || !log.date || !log.sets) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Please fill all the fields" });
+  }
+
+  const newLog = ExerciseLog(log);
+
+  try {
+    await newLog.save();
+    res.status(200).json({ success: true, data: newLog });
+  } catch (error) {
+    console.log("Error in LogCreate");
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const deleteLog = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Log id not found" });
+  }
+
+  try {
+    await ExerciseLog.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Log Deleted" });
+  } catch (error) {
+    console.log("Error in log delete");
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const getLatestLogForExercise = async (req, res) => {
+  const { exerciseId } = req.params;
+  try {
+    const logs = await ExerciseLog.find({ exercise: exerciseId }).sort({
+      date: -1,
+    });
+
+    if (!logs) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Log is not found" });
+    }
+    res.status(200).json({ success: true, data: logs[0] });
+  } catch (error) {
+    console.log("Error in get log", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
